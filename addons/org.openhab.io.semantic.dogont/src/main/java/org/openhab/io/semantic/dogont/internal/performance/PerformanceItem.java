92960c29325112087d6df223149d332387f652ef
andiikaa <andre.kuehnie@gmail.com>
2019-06-13 09:53:56 +0200

    a little bit working with old build system

--- addons/org.openhab.io.semantic.dogont/src/main/java/org/openhab/io/semantic/dogont/internal/performance/PerformanceItem.java
+++ addons/org.openhab.io.semantic.dogont/src/main/java/org/openhab/io/semantic/dogont/internal/performance/PerformanceItem.java
@@ -1,10 +1,15 @@
 package org.openhab.io.semantic.dogont.internal.performance;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.core.items.Item;
+import org.eclipse.smarthome.core.library.types.StringType;
 import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.State;
 import org.eclipse.smarthome.core.types.StateDescription;
@@ -18,12 +23,7 @@ public class PerformanceItem implements Item {
 
     @Override
     public State getState() {
-        return null;
-    }
-
-    @Override
-    public State getStateAs(Class<? extends State> typeClass) {
-        return null;
+        return StringType.EMPTY;
     }
 
     @Override
@@ -33,27 +33,28 @@ public class PerformanceItem implements Item {
 
     @Override
     public String getType() {
-        return null;
+        return "";
     }
 
     @Override
     public List<Class<? extends State>> getAcceptedDataTypes() {
-        return null;
+        return new ArrayList<Class<? extends State>>();
+
     }
 
     @Override
     public List<Class<? extends Command>> getAcceptedCommandTypes() {
-        return null;
+        return new ArrayList<Class<? extends Command>>();
     }
 
     @Override
     public List<String> getGroupNames() {
-        return null;
+        return new ArrayList<String>();
     }
 
     @Override
     public Set<String> getTags() {
-        return null;
+        return new HashSet<String>();
     }
 
     @Override
@@ -84,6 +85,11 @@ public class PerformanceItem implements Item {
     @Override
     public String getUID() {
         return "UID_for_" + name;
+    }
+
+    @Override
+    public <T extends @NonNull State> @Nullable T getStateAs(Class<T> typeClass) {
+        return null;
     }
 
 }
